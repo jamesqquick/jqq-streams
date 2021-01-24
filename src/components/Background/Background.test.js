ab3fa015f3a2ad6857c41ee832b2b4abaf4b2bda
James Quick <james.q.quick@outlook.com>
2021-01-23 20:26:13 -0600

    Removed tests and plop

--- src/components/Background/Background.test.js
+++
@@ -1,7 +0,0 @@
-import Background from "components/Background"
-import {render, screen} from "test-utils/render"
-
-test("shows background", () => {
-    render(<Background />)
-    expect(screen.getByAltText("liquid"))
-})
--- src/components/Video/Video.test.js
+++
@@ -1,7 +0,0 @@
-import Video from "components/Video"
-import {render, screen} from "test-utils/render"
-
-test("shows video", () => {
-    render(<Video>testing</Video>)
-    expect(screen.getByText("testing"))
-})
