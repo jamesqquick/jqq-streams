7379ed228d477e7ab91c47516a799f0ab0d3a03b
James Quick <james.q.quick@outlook.com>
2021-03-03 08:27:33 -0600

    remove reference to video component

--- src/pages/coding.js
+++ src/pages/coding.js
@@ -3,21 +3,6 @@ import Background from "components/Background"
 import Footer from "components/Footer"
 import SocialBar from "components/SocialBar"
 import {PrimaryVideo, SecondaryVideo} from "components/Video"
-import styled from "styled-components"
-
-const HostScreen = styled(Video)`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-`
-
-const HostWebcam = styled(Video)`
-    position: absolute;
-    right: 4rem;
-    bottom: calc(20px);
-    z-index: 1;
-`
 
 const CodingPage = () => {
     return (
